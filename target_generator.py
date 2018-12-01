db97cb09f90591e6cb55f2ac9605698b0bbfcaac
Ingyo Chung <jik0730s@berkeley.edu>
2018-12-02 00:06:52 +0900

    add random walk data type

--- target_generator.py
+++ target_generator.py
@@ -69,7 +69,7 @@ def sin_freq_variation(freq_from, freq_to, sys_freq, simT, sine_type=None):
 
 
 def random_walk(T, data_type):
-    split = sine_type.split('_')
+    split = data_type.split('_')
     max_degree = float(split[2][:-3]) / 180. * math.pi
     SEED = int(split[2][:-4])
 
