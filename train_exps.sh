36557f949b6e94270b7afbd50c09d7c2137d11f1
Ingyo Chung <jik0730s@berkeley.edu>
2018-12-03 14:39:17 +0900

    add sine freq variation test

--- train_exps.sh
+++ train_exps.sh
@@ -29,7 +29,7 @@
 # python main.py --ftype 1 --simT 10 --model_dir exp/random_walk --data_type random_walk_30deg_3seed &
 # python main.py --ftype 2 --simT 10 --model_dir exp/random_walk --data_type random_walk_30deg_3seed &
 # wait
-python main.py --ftype 0 --simT 13.15 --model_dir exp/random_walk --data_type sine_freq_variation &
-python main.py --ftype 1 --simT 13.15 --model_dir exp/random_walk --data_type sine_freq_variation &
-python main.py --ftype 2 --simT 13.15 --model_dir exp/random_walk --data_type sine_freq_variation &
+python main.py --ftype 0 --simT 13 --model_dir exp/random_walk --data_type sine_freq_variation &
+python main.py --ftype 1 --simT 13 --model_dir exp/random_walk --data_type sine_freq_variation &
+python main.py --ftype 2 --simT 13 --model_dir exp/random_walk --data_type sine_freq_variation &
 wait
